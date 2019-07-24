e70d7693c355d3053fbb5e33665b4a2aad816310
boazy <boazy@126.com>
2019-07-25 00:26:57 +0800

    按dubbo 2.7.3 实现dubbo-demo-provider/dubbo-demo-consumer

--- boazy-dubbo-demo-consumer/src/main/java/boazy/dubbo/demo/consumer/ConsumerApp.java
+++ boazy-dubbo-demo-consumer/src/main/java/boazy/dubbo/demo/consumer/ConsumerApp.java
@@ -1,71 +1,67 @@
 package boazy.dubbo.demo.consumer;
 
-import java.io.IOException;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.alibaba.dubbo.rpc.RpcContext;
 import boazy.dubbo.demo.api.DubboUserService;
 import boazy.dubbo.demo.api.HessianUserService;
 import boazy.dubbo.demo.api.RestUserService;
 import boazy.dubbo.demo.api.RmiUserService;
 import boazy.dubbo.demo.api.User;
+import org.apache.dubbo.rpc.RpcContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
 
 /**
- *
- * @Company boazy
- * @author boazy
- * @Date Jul 10, 2018
- *
+ * @author boazy@126.com
+ * @date Jul 10, 2018
  */
 public class ConsumerApp {
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
-		System.out.println("dubbo consumer start...");
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "classpath*:/META-INF/spring/*.xml" });
-		context.start();
-		
-		try {
-			DubboUserService dubboUserService = (DubboUserService) context.getBean("dubboUserService");
-			User user = dubboUserService.queryUser("dubboUser");
-			System.out.println(user.getName());
-			System.out.println(user.getEmail());
-			System.out.println(user.getTel());
-			dubboUserService.registerUser(new User("reg.dubboUser", "80808080-8081", "reg.dubboUser@126.com"));
-		} catch (Exception e) {
-			System.out.println("dubboUserService invoking error.");
-			e.printStackTrace();
-		}
-		
-		try {
-			HessianUserService hessianUserService = (HessianUserService) context.getBean("hessianUserService");
-			RpcContext.getContext();
-			hessianUserService.registerUser(new User("reg.hessianUser", "80808080-8082", "hessianUser@126.com"));
-		} catch (Exception e) {
-			System.out.println("hessianUserService invoking error.");
-			e.printStackTrace();
-		}
-		
-		try {
-			RmiUserService rmiUserService = (RmiUserService) context.getBean("rmiUserService");
-			rmiUserService.registerUser(new User("reg.rmiUser", "80808080-8084", "rmiUser@126.com"));
-		} catch (Exception e) {
-			System.out.println("rmiUserService invoking error.");
-			e.printStackTrace();
-		}
-		
-		try {
-			RestUserService restUserService = (RestUserService) context.getBean("restUserService");
-			restUserService.registerUser(new User("reg.restUser", "80808080-8083", "restUser@126.com"));
-		} catch (Exception e) {
-			System.out.println("restUserService invoking error.");
-			e.printStackTrace();
-		}
-		
-		System.out.println("dubbo consumer end.");
-		System.in.read();
-	}
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws IOException {
+        System.out.println("dubbo consumer start...");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"classpath*:/META-INF/spring/*.xml"});
+        context.start();
+
+        try {
+            DubboUserService dubboUserService = (DubboUserService) context.getBean("dubboUserService");
+            User user = dubboUserService.queryUser("dubboUser");
+            System.out.println(user.getName());
+            System.out.println(user.getEmail());
+            System.out.println(user.getTel());
+            dubboUserService.registerUser(new User("reg.dubboUser", "80808080-8081", "reg.dubboUser@126.com"));
+        } catch (Exception e) {
+            System.out.println("dubboUserService invoking error.");
+            e.printStackTrace();
+        }
+
+        try {
+            HessianUserService hessianUserService = (HessianUserService) context.getBean("hessianUserService");
+            RpcContext.getContext();
+            hessianUserService.registerUser(new User("reg.hessianUser", "80808080-8082", "hessianUser@126.com"));
+        } catch (Exception e) {
+            System.out.println("hessianUserService invoking error.");
+            e.printStackTrace();
+        }
+
+        try {
+            RmiUserService rmiUserService = (RmiUserService) context.getBean("rmiUserService");
+            rmiUserService.registerUser(new User("reg.rmiUser", "80808080-8084", "rmiUser@126.com"));
+        } catch (Exception e) {
+            System.out.println("rmiUserService invoking error.");
+            e.printStackTrace();
+        }
+
+        try {
+            RestUserService restUserService = (RestUserService) context.getBean("restUserService");
+            restUserService.registerUser(new User("reg.restUser", "80808080-8083", "restUser@126.com"));
+        } catch (Exception e) {
+            System.out.println("restUserService invoking error.");
+            e.printStackTrace();
+        }
+
+        System.out.println("dubbo consumer end.");
+        System.in.read();
+    }
 
 }
--- boazy-dubbo-demo-provider/src/main/java/boazy/dubbo/demo/provider/RestUserServiceImpl.java
+++ boazy-dubbo-demo-provider/src/main/java/boazy/dubbo/demo/provider/RestUserServiceImpl.java
@@ -3,36 +3,17 @@ package boazy.dubbo.demo.provider;
 import boazy.dubbo.demo.api.RestUserService;
 import boazy.dubbo.demo.api.User;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.dubbo.rpc.protocol.rest.support.ContentType;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 
 /**
  * @author boazy@126.com
  * @date Jul 10, 2018
  */
-@Path("user-srv")
 public class RestUserServiceImpl implements RestUserService {
 
-    @GET
-    @Path("query")
-    @Produces({ContentType.APPLICATION_JSON_UTF_8})
-    @Override
-    public User queryUser(@QueryParam("name") String name) {
+    public User queryUser(String name) {
         return new User(name, "8083", "rest.user.srv@126.com");
     }
 
-    @POST
-    @Path("register")
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({ContentType.APPLICATION_JSON_UTF_8})
-    @Override
     public String registerUser(User user) {
         if (null == user) {
             System.err.println("null rest.user.");
