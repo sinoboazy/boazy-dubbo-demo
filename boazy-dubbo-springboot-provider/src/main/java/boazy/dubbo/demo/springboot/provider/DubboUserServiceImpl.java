ff6861ee2706f3fe0705b6da7575c5ce83d01c14
boazy <boazy@126.com>
2019-07-27 23:45:28 +0800

    实现spring boot provider

--- boazy-dubbo-springboot-provider/src/main/java/boazy/dubbo/demo/springboot/provider/DubboUserServiceImpl.java
+++ boazy-dubbo-springboot-provider/src/main/java/boazy/dubbo/demo/springboot/provider/DubboUserServiceImpl.java
@@ -3,34 +3,30 @@ package boazy.dubbo.demo.springboot.provider;
 import boazy.dubbo.demo.api.DubboUserService;
 import boazy.dubbo.demo.api.User;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
- *
- * @Company boazy
- * @author boazy
- * @Date Jul 10, 2018
- *
+ * @author boazy@126.com
+ * @date Jul 10, 2018
  */
+@Service(protocol = "dubbo")
 public class DubboUserServiceImpl implements DubboUserService {
 
-	public DubboUserServiceImpl() {
-	}
+    @Override
+    public User queryUser(String name) {
+        return new User(name, "8091", "springboot.dubbo.user.srv@126.com");
+    }
 
-	@Override
-	public User queryUser(String name) {
-		return new User(name, "8091", "springboot.dubbo.user.srv@126.com");
-	}
-
-	@Override
-	public String registerUser(User user) {
-		if (null == user) {
-			System.err.println("null springboot.dubbo.user.");
-			return "{}";
-		}
-		System.out.println("springboot.dubbo.user.username=" + (null != user ? user.getName() : null));
-		System.out.println("springboot.dubbo.user.email=" + (null != user ? user.getEmail() : null));
-		System.out.println("springboot.dubbo.user.tel=" + (null != user ? user.getTel() : null));
-		return JSONObject.toJSONString(user);
-	}
+    @Override
+    public String registerUser(User user) {
+        if (null == user) {
+            System.err.println("null springboot.dubbo.user.");
+            return "{}";
+        }
+        System.out.println("springboot.dubbo.user.username=" + user.getName());
+        System.out.println("springboot.dubbo.user.email=" + user.getEmail());
+        System.out.println("springboot.dubbo.user.tel=" + user.getTel());
+        return JSONObject.toJSONString(user);
+    }
 
 }
--- boazy-dubbo-springboot-provider/src/main/java/boazy/dubbo/demo/springboot/provider/ProviderSpringBootApp.java
+++ boazy-dubbo-springboot-provider/src/main/java/boazy/dubbo/demo/springboot/provider/ProviderSpringBootApp.java
@@ -1,27 +1,19 @@
-/**
- * 
- */
 package boazy.dubbo.demo.springboot.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 /**
- * 
- * @Company boazy
- * @author boazy
- * @Date Jul 10, 2018
- *
+ * @author boazy@126.com
+ * @date Jul 10, 2018
  */
-@SpringBootApplication
-@ImportResource({"classpath:/dubbo-demo-provider.xml"})
+@EnableAutoConfiguration
 public class ProviderSpringBootApp {
 
-	public static void main(String[] args) {
-		System.out.println("dubbo springboot provider start...");
-		SpringApplication.run(ProviderSpringBootApp.class, args);
-		System.out.println("dubbo springboot provider started.");
-	}
+    public static void main(String[] args) {
+        System.out.println("dubbo springboot provider start...");
+        SpringApplication.run(ProviderSpringBootApp.class, args);
+        System.out.println("dubbo springboot provider started.");
+    }
 
 }
