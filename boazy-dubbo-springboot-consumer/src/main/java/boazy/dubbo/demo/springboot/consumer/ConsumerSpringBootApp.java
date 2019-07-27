4d225394c5d38d9e8c1c04a58dce4eb95b26be14
boazy <boazy@126.com>
2019-07-28 00:04:29 +0800

    实现spring boot consumer

--- boazy-dubbo-springboot-consumer/src/main/java/boazy/dubbo/demo/springboot/consumer/ConsumerSpringBootApp.java
+++ boazy-dubbo-springboot-consumer/src/main/java/boazy/dubbo/demo/springboot/consumer/ConsumerSpringBootApp.java
@@ -3,54 +3,54 @@
  */
 package boazy.dubbo.demo.springboot.consumer;
 
-import java.io.IOException;
-
+import boazy.dubbo.demo.api.DubboUserService;
 import boazy.dubbo.demo.api.RestUserService;
+import boazy.dubbo.demo.api.User;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
-
-import boazy.dubbo.demo.api.DubboUserService;
-import boazy.dubbo.demo.api.User;
+import org.springframework.context.annotation.Bean;
 
 /**
- * 
- * @Company boazy
- * @author boazy
- * @Date Jul 10, 2018
- *
+ * @author boazy@126.com
+ * @date Jul 10, 2018
  */
 @SpringBootApplication
-@ImportResource({"classpath:/dubbo-demo-consumer.xml"})
 public class ConsumerSpringBootApp {
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("dubbo springboot consumer start...");
-		ApplicationContext ctx = SpringApplication.run(ConsumerSpringBootApp.class, args);
-		
-		try {
-			DubboUserService dubboUserService = (DubboUserService) ctx.getBean("dubboUserService");
-			User user = dubboUserService.queryUser("springboot.dubboUser");
-			System.out.println(user.getName());
-			System.out.println(user.getEmail());
-			System.out.println(user.getTel());
-			dubboUserService.registerUser(new User("springboot.reg.dubboUser", "80088008-8091", "springboot.reg.dubboUser.job"));
-		} catch (Exception e) {
-			System.out.println("dubboUserService invoking error.");
-			e.printStackTrace();
-		}
+	@Reference(protocol = "dubbo")
+	private DubboUserService dubboUserService;
+	@Reference(protocol = "rest")
+	private RestUserService restUserService;
 
-		try {
-			RestUserService restUserService = (RestUserService) ctx.getBean("restUserService");
-			restUserService.registerUser(new User("springboot.reg.restUser", "80088008-8093", "springboot.restUser@126.com"));
-		} catch (Exception e) {
-			System.out.println("restUserService invoking error.");
-			e.printStackTrace();
-		}
-		
+	public static void main(String[] args) {
+		System.out.println("dubbo springboot consumer start...");
+		SpringApplication.run(ConsumerSpringBootApp.class, args);
 		System.out.println("dubbo springboot consumer end.");
-		System.in.read();
+	}
+
+	@Bean
+	public ApplicationRunner runner() {
+		return args -> {
+			try {
+				User user = dubboUserService.queryUser("springboot.dubboUser");
+				System.out.println(user.getName());
+				System.out.println(user.getEmail());
+				System.out.println(user.getTel());
+				dubboUserService.registerUser(new User("springboot.reg.dubboUser", "80088008-8091", "springboot.reg.dubboUser.job"));
+			} catch (Exception e) {
+				System.out.println("dubboUserService invoking error.");
+				e.printStackTrace();
+			}
+
+			try {
+				restUserService.registerUser(new User("springboot.reg.restUser", "80088008-8093", "springboot.restUser@126.com"));
+			} catch (Exception e) {
+				System.out.println("restUserService invoking error.");
+				e.printStackTrace();
+			}
+		};
 	}
 
 }
